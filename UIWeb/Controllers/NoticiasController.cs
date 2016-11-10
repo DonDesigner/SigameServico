1e9d359ed0d842035fd59bf50164d93e719a71c4
Diego Fernandes <dondesigner@hotmail.com>
2016-11-09 23:59:08 -0200

    Adicionado Editar e Delelar em Noticias

--- UIWeb/Controllers/NoticiasController.cs
+++ UIWeb/Controllers/NoticiasController.cs
@@ -3,6 +3,7 @@ using Services.Noticias;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -38,5 +39,36 @@ namespace UIWeb.Controllers
             }
             return View();
         }
+
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+
+            Noticia noticia = ns.Listardados().Find(x => x.Id == id);
+            if (noticia == null)
+            {
+                return HttpNotFound();
+            }
+            return View(noticia);
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            if(id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Noticia noticia = ns.Listardados().Find(x => x.Id == id);
+            if(noticia == null)
+            {
+                return HttpNotFound();
+            }
+            return View(noticia);
+        }
+
+
     }
 }
